7a52415333adf76b5c09e16ca36c17d6e7a61a61
Johnatan Borges <johnatanborges@hotmail.com>
2021-02-21 14:02:03 -0300

    feat: passing method props

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
 
     const [otherState, setOtherState] = useState('some other value');
 
-    const switchNameHandler = () => {
+    const switchPersonHandler = (newName) => {
         setPersons([
-            { name: 'Maximilian', age: 30 },
+            { name: newName, age: 30 },
             { name: 'Manu', age: 29 },
             { name: 'Ana', age: 26 },
         ]);
@@ -27,9 +27,9 @@ function App() {
         <div className="App">
             <h1>Hi, I'm a react component</h1>
             <p>This is really working!</p>
-            <button onClick={switchNameHandler}>Switch Name</button>
+            <button onClick={ () => switchPersonHandler('Maximilian')}>Switch Name</button>
             <Person name={persons[0].name} age={persons[0].age} />
-            <Person name={persons[1].name} age={persons[1].age} >My Hobbies: Racing</Person>
+            <Person name={persons[1].name} age={persons[1].age} click={switchPersonHandler.bind(this, 'John')} >My Hobbies: Racing</Person>
             <Person name={persons[2].name} age={persons[2].age} />
         </div>
     );
@@ -47,7 +47,7 @@ function App() {
 //         otherState: 'Some other value',
 //     }
 
-//     switchNameHandler = () => {
+//     switchPersonHandler = () => {
 //         this.setState({
 //             persons: [
 //                 { name: 'Maximilian', age: 30 },
@@ -62,7 +62,7 @@ function App() {
 //             <div className="App">
 //                 <h1>Hi, I'm a react component</h1>
 //                 <p>This is really working!</p>
-//                 <button onClick={this.switchNameHandler}>Switch Name</button>
+//                 <button onClick={this.switchPersonHandler}>Switch Name</button>
 //                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
 //                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
 //                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
--- src/Components/Person.jsx
+++ src/Components/Person.jsx
@@ -1,6 +1,7 @@
 const Person = (props) => {
+    console.log(props.click)
     return (
-        <div>
+        <div onClick={props.click}>
             <p>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
         </div>
